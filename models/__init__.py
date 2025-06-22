383327c1e2de80bb15f847daa7b60faf0268d62a
Tv772-ut <niuzheng775@gmail.com>
2025-06-23 03:45:38 +0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,12 @@
-from .base import Base
-from .group import Group
-from .record import Record
-from .wallet import Wallet
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from utils.config import DATABASE_PATH
+
+engine = create_engine(f'sqlite:///{DATABASE_PATH}', echo=False)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def init_db():
+    from .models import Group, Record, UserWallet
+    Base.metadata.create_all(engine)
